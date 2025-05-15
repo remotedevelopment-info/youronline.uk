bb74cedfe6fd711522f1928fbf519880b4ebcfb3
Nicholas Alexander <info@remotedevelopment.co.uk>
2025-05-15 08:15:42 +0100

    update svelte config to fallback to index

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,15 @@ import adapter from '@sveltejs/adapter-vercel';
  
 export default {
   kit: {
-    adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true,
+			strict: true
+		})
   },
 };
 // import adapter from '@sveltejs/adapter-static';
