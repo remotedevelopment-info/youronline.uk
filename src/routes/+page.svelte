7b6f36b8aeaa05784403c9b881bef2259b02cc27
Nicholas Alexander <info@remotedevelopment.co.uk>
2025-05-15 12:53:17 +0100

    meeting cta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -85,6 +85,23 @@
 		<PricingGrid plans={pricingPlans} />
 	</section>
 
+	<div class="bg-black">
+		<h2 class="text-center">Book a FREE discovery chat...</h2>
+		<div class="mt-4">
+			<!-- Calendly inline widget begin -->
+			<div
+				class="calendly-inline-widget"
+				data-url="https://calendly.com/youronlineuk/discovery-chat"
+				style="min-width:320px;height:700px;"
+			></div>
+			<script
+				type="text/javascript"
+				src="https://assets.calendly.com/assets/external/widget.js"
+				async
+			></script>
+			<!-- Calendly inline widget end -->
+		</div>
+	</div>
 	<!-- Contact Section -->
 	<section id="contact">
 		<Contact />
